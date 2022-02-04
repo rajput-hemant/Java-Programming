f2f84d313b81f9da9edd0951057241848350d786
Hemant Rajput <rajput.hemant2001@gmail.com>
2022-02-04 18:16:04 +0530

    Code Push

--- dsa/recursion/PowerFn.java
+++ dsa/recursion/PowerFn.java
@@ -1,14 +1,28 @@
 package recursion;
 
+/*
+Write a program to find x to the power n (i.e. x^n). Take x and n from the user. You need to return the answer.
+Do this recursively.
+Input format :
+Two integers x and n (separated by space)
+Output Format :
+x^n (i.e. x raise to the power n)
+Sample Input 1 :
+3 4
+Sample Output 1 :
+81
+*/
+
 import java.util.Scanner;
 
 public class PowerFn {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the vlaues for X and N i.e x^n -> ");
+        System.out.print("Enter the values for X and N i.e x^n -> ");
         int x = sc.nextInt();
         int n = sc.nextInt();
-        System.out.print(x + " raise to the power " + n + " is -> " + power(x, n));
+        System.out.print(power(x, n));
+        // System.out.print(x + " raise to the power " + n + " is -> " + power(x, n));
         sc.close();
     }
     // This recursion function of power has Stack Height -> n
